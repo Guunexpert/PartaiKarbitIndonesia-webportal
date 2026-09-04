b2a2fc27c421e10a897ddade43ab5752061d111e
guunexpert <140092536+Guunexpert@users.noreply.github.com>
2026-09-04 10:42:58 +0700

    Add files via upload

---
+++ src/config.ts
@@ -0,0 +1,36 @@
+const config = {
+
+  serverName: 'YOURSERVER',
+
+  logo: '/logo.png',
+
+  backgroundImage: '/bg.png',
+
+  hueColor: '#3b82f6',
+
+  links: [
+    {
+      label: 'Map',
+      href: 'https://map.yourserver.com',
+      color: '#6b3fa8',
+    },
+    {
+      label: 'Store',
+      href: 'https://store.yourserver.com',
+      color: '#2d8c52',
+    },
+    {
+      label: 'Rules',
+      href: 'https://yourserver.com/rules',
+      color: '#c04444',
+    },
+    {
+      label: 'Discord',
+      href: 'https://discord.gg/yourserver',
+      color: '#2d6fa8',
+    },
+  ],
+
+};
+
+export default config;
